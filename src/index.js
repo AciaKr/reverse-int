3aab19dd6ffd5667089a88fc39154ad4e135ced4
AciaKr <krasnastassia@gmail.com>
2023-01-27 17:50:50 +0300

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+ module.exports = function reverse (n) {
+        let stringOfN = String(Math.abs(n));
+        let i = 0;
+        let result ='';
+        while (i < stringOfN.length) {
+            result =  stringOfN[i] + result;
+            i += 1;
+        }
+        return +result;
 }
